d58a3c6ca15e02287c365272e9ca050962db42da
Piotr Truszkowski <truszkowski@gmail.com>
2023-05-31 11:07:37 +0200

    main.tf

---
+++ main.tf
@@ -0,0 +1,16 @@
+resource "random_password" "secret_1" {
+  length  = 30
+  special = true
+}
+
+resource "random_password" "secret_2" {
+  length  = 30
+  special = true
+}
+
+resource "random_password" "secret_3" {
+  length  = 30
+  special = true
+}
+
+
